e8b55ac70139997d12d9066ffff86ff4ac1243c7
idofogel <ido.moshe.fogel@gmail.com>
2021-05-27 00:04:27 +0300

    Create index.js

---
+++ find_prod/react-redux-tutorial/src/js/actions/index.js
@@ -0,0 +1,15 @@
+export function addDoctor(payload) {
+  return { type: "ADD_DOCTOR", payload }
+};
+// export function addArticle(payload) {
+//   return { type: "ADD_ARTICLE", payload }
+// };
+export function getData() {
+  return function(dispatch) {
+    return fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(json => {
+        dispatch({ type: "DATA_LOADED", payload: json });
+      });
+  };
+}
